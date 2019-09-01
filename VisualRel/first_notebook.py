17ec31ef36f54dd5a392bcdbaa6d6648dfa8c026
Catalin Perticas <perticas_catalin@yahoo.com>
2019-09-01 20:03:41 +0300

    pandas search

--- VisualRel/first_notebook.py
+++ VisualRel/first_notebook.py
@@ -27,31 +27,61 @@ print ('finish load data')
 
 # Any results you write to the current directory are saved as output.
 
-print (df_rel['RelationshipLabel'].value_counts())
+#print (df_rel['RelationshipLabel'].value_counts())
 #print (df_rel['LabelName1'].value_counts())
 #print (df_rel['LabelName2'].value_counts())
 df_rel['ImageID'].value_counts()
 df_rel.sample(5)
 #df_rel['LabelName1'].head()
 
-class_name_dict = {}
+df_classes = pd.read_csv('./input/challenge-2019-classes-vrd.csv')
 
-def map_to_name(label_name):
-    #class_name = class_name_dict(label_name)
-    class_name = 'beer'
-    choice = random.choice('ab')
-    if choice == 'a':
-        class_name = 'milk'
-    return class_name
+print (df_classes)
 
-print (map_to_name('alpha'))
-print (map_to_name('beta'))
-print (map_to_name('gamma'))
+class_name_dict = dict(zip(df_classes.Label,df_classes.LabelName))
 
-same_thing_repeated = df_rel[['LabelName2']][df_rel.LabelName2 == '/m/083vt']
+print (class_name_dict)
+print (len(class_name_dict))
+
+def map_to_name(label):
+	#return isinstance(label, str)
+	#return label.astype('str')
+
+	# if not isinstance(label, str):
+	# 	label = label.astype('str')
+
+	if label in class_name_dict.keys():
+		return class_name_dict[label]
+
+	return 'milk'
+    #class_name = class_name_dict[label_name]
+
+    # class_name = 'beer'
+    # choice = random.choice('ab')
+    # if choice == 'a':
+    #     class_name = 'milk'
+    # return class_name
+
+# Not existing
+# print (map_to_name('alpha'))
+# print (map_to_name('beta'))
+# print (map_to_name('gamma'))
+
+# Should be, but is not
+#print (map_to_name('/m/083vt'))
+#same_thing_repeated = df_rel[['LabelName2']][df_rel.LabelName2 == '/m/083vt']
+#print(same_thing_repeated.head())
+
+print (map_to_name('/m/04dr76w'))
+
+same_thing_repeated = df_rel[['LabelName1', 'LabelName2', 'RelationshipLabel']][df_rel.LabelName2 == '/m/04dr76w']
 print(same_thing_repeated.head())
-translated = same_thing_repeated.apply(lambda x : map_to_name(x), axis = 1)
-print(translated.head())
+
+# not working start here
+translated = same_thing_repeated
+translated['LabelName1'] = translated[['LabelName1']].apply(lambda x : map_to_name(x.all()), axis = 1)
+translated['LabelName2'] = translated[['LabelName2']].apply(lambda x : map_to_name(x.all()), axis = 1)
+print(translated.sample(50))
 
 #numerical = ['XMin1', 'XMax1', 'YMin1', 'YMax1', 'XMin2', 'XMax2', 'YMin2', 'YMax2']
 #df_rel[numerical].hist(bins=15, figsize=(20, 10), layout=(2, 4));
