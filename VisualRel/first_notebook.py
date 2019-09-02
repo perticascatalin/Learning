589b5afa7b224ccdf8ab2a214a05faf942f10a47
Catalin Perticas <perticas_catalin@yahoo.com>
2019-09-02 15:10:00 +0300

    structured functionality

--- VisualRel/first_notebook.py
+++ VisualRel/first_notebook.py
@@ -1,61 +1,118 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import random
+import os # listing files and directories
 
-import os
+header_index = 0
 
-for dirname, _, filenames in os.walk('./input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+def print_header(header):
+    global header_index
+    print ('====================================')
+    print (str(header_index) + '. ' + header)
+    print ('====================================')
+    header_index += 1
+
+def print_subheader(header):
+    print (header)
+    print ('------------------------------------')
+
+def walk_the_input():
+    print_header("WALK THE INPUT:")
+    for dirname, _, filenames in os.walk('./input'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+
+def map_to_name(label):
+    # check in class labels
+    if label in class_name_dict.keys():
+        return class_name_dict[label]
+
+    # check in attr labels
+    if label in attr_name_dict.keys():
+        return attr_name_dict[label]
+
+    return 'milk'
+
+def map_to_tag(name):
+    # check in classes
+    if label in classes.keys():
+        return classes[label]
+
+    # check in attributes
+    if label in attributes.keys():
+        return attributes[label]
+
+    return 'unknown'
+
+# START PROGRAM
+
+walk_the_input()
+print_header("READ & PRINT TABLES:")
 
 df_rel = pd.read_csv('./input/challenge-2019-train-vrd.csv')
-#print (df_rel['RelationshipLabel'].value_counts())
 
+#print (df_rel['RelationshipLabel'].value_counts())
+#df_rel['RelationshipLabel'].head()
 #print (df_rel['LabelName1'].value_counts())
 #df_rel['LabelName1'].head()
 #print (df_rel['LabelName2'].value_counts())
-
 #df_rel['LabelName2'].head()
+
 df_rel['ImageID'].value_counts()
 df_rel.sample(5)
 
+print_subheader('CLASSES:')
 df_classes = pd.read_csv('./input/challenge-2019-classes-vrd.csv')
 print (df_classes)
 
+print_subheader('ATTR:')
 df_attr = pd.read_csv('./input/challenge-2019-attributes-description.csv')
-print (df_attr)
+print(df_attr)
 
-class_name_dict = dict(zip(df_classes.Label,df_classes.LabelName))
-attr_name_dict = dict(zip(df_attr.Label,df_attr.LabelName))
+class_name_dict = dict(zip(df_classes.Label, df_classes.LabelName))
+attr_name_dict = dict(zip(df_attr.Label, df_attr.LabelName))
+classes = dict(zip(df_classes.LabelName, df_classes.Label))
+attributes = dict(zip(df_attr.LabelName, df_attr.Label))
 
 #print (class_name_dict)
 #print (len(class_name_dict))
 #print (attr_name_dict)
 #print (len(attr_name_dict))
 
-def map_to_name(label):
-	#return isinstance(label, str)
-	#return label.astype('str')
-
-	if label in class_name_dict.keys():
-		return class_name_dict[label]
-
-	if label in attr_name_dict.keys():
-		return attr_name_dict[label]
-
-	return 'milk'
-    #class_name = class_name_dict[label]
-
-    # class_name = 'beer'
-    # choice = random.choice('ab')
-    # if choice == 'a':
-    #     class_name = 'milk'
-    # return class_name
-
 # Not existing
 # print (map_to_name('alpha'))
 # print (map_to_name('beta'))
 # print (map_to_name('gamma'))
+
+def list_class_labels():
+    print_header("CLASS LABELS:")
+    for key, val in class_name_dict.items():
+        print (str(key) + ' ' + str(val))
+    print('')
+
+def list_attr_labels():
+    print_header("ATTR LABELS:")
+    for key, val in attr_name_dict.items():
+        print (str(key) + ' ' + str(val))
+    print('')
+
+def list_classes():
+    print_header("CLASS NAMES:")
+    for key, val in classes.items():
+        print (str(key) + ' ' + str(val))
+
+def list_attributes():
+    print_header("ATTR NAMES:")
+    for key, val in attributes.items():
+        print (str(key) + ' ' + str(val))
+
+#list_class_labels()
+#list_attr_labels()
+list_classes()
+list_attributes()
+exit()
+
+# SEARCH PART
 
 # complement style (label name 2)
 TARGET_LABEL = '/m/04dr76w' # Bottle
