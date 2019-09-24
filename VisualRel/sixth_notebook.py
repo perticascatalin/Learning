f9af41928e62ea716fd3c2f8923834c5a57b8b26
Catalin Perticas <perticas_catalin@yahoo.com>
2019-09-24 22:12:35 +0300

    decrease nn size

--- VisualRel/sixth_notebook.py
+++ VisualRel/sixth_notebook.py
@@ -40,13 +40,13 @@ def conv_net(x, num_classes, num_labels, dropout, reuse, is_training):
     # Define a scope for reusing the variables
     with tf.variable_scope('ConvNet', reuse=reuse):
 
-        # Convolution Layer with 32 filters and a kernel size of 5
-        conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
+        # Convolution Layer with 16 filters and a kernel size of 5
+        conv1 = tf.layers.conv2d(x, 16, 5, activation=tf.nn.relu)
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
 
-        # Convolution Layer with 64 filters and a kernel size of 3
-        conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
+        # Convolution Layer with 32 filters and a kernel size of 3
+        conv2 = tf.layers.conv2d(conv1, 32, 3, activation=tf.nn.relu)
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
 
@@ -54,7 +54,7 @@ def conv_net(x, num_classes, num_labels, dropout, reuse, is_training):
         fc1 = tf.contrib.layers.flatten(conv2)
 
         # Fully connected layer (in contrib folder for now)
-        fc1 = tf.layers.dense(fc1, 512)
+        fc1 = tf.layers.dense(fc1, 256)
         # Apply Dropout (if is_training is False, dropout is not applied)
         fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
 
