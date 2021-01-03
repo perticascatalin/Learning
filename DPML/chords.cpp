3adeebd711fea55dea186710c7070f6fe81ec6aa
Catalin Perticas <perticas_catalin@yahoo.com>
2021-01-03 14:46:42 +0200

    create chords

--- DPML/chords.cpp
+++ DPML/chords.cpp
@@ -16,6 +16,8 @@ vector <string> c_major_ch;
 int c_major_num;
 int L2 = 7;
 
+map <string, vector<string> > chord_notes;
+
 double double_mod(double num, int m) {
 	int inum = int(num);
 	double rem = num - inum;
@@ -26,6 +28,7 @@ void read_input() {
 	ifstream cin1("ch_pos.in");
 	ifstream cin2("c_major_ch.in");
 
+	cout << "\n" << "1. Diatonic & chromatic notes mapped to their absolute position" << "\n\n";
 	cin1 >> ch_num;
 	for (int i = 0; i < ch_num; ++i) {
 		string ch; double pos;
@@ -35,6 +38,7 @@ void read_input() {
 		pos_ch[pos] = ch;
 	}
 
+	cout << "\n" << "2. Chords (& roots) in C Major scale" << "\n\n";
 	cin2 >> c_major_num;
 	for (int i = 0; i < c_major_num; ++i) {
 		string step;
@@ -50,8 +54,27 @@ void read_input() {
 		cout << c_major_ch[i] << "\n";
 }
 
+void create_chords() {
+	cout << "\n" << "3. Chord notes (root, third & fifth)" << "\n\n";
+	for (int i = 0; i < c_major_num; ++i) {
+		cout << c_major_ch[i] << "\n";
+		vector <string> ch_notes;
+		// Add root
+		ch_notes.push_back(c_major_step[i]);
+		// Add third
+		ch_notes.push_back(c_major_step[(i+2) % L2]);
+		// Add fifth
+		ch_notes.push_back(c_major_step[(i+4) % L2]);
+		for (int j = 0; j < ch_notes.size(); ++j)
+			cout << ch_notes[j] << " ";
+		cout << "\n";
+		chord_notes[c_major_ch[i]] = ch_notes;
+	}
+}
+
 int main() {
 	read_input();
+	create_chords();
 	// Test double mod
 	// cout << double_mod(7.75, L1) << "\n";
 
